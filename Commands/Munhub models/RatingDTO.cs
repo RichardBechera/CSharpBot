75aec480632f24c1dcb5d012523a5db9a2c22de6
Richard Bechera <richard@niphilim.localdomain>
2021-01-06 15:48:56 +0100

    teacher rating

--- Commands/Munhub models/RatingDTO.cs
+++ Commands/Munhub models/RatingDTO.cs
@@ -9,13 +9,19 @@ namespace csharpbot.Commands.Munhub_models
         public string Negatives { get; set; }
 
         public byte Rating { get; set; }
+        
+        public int Teacher { get; set; }
 
-        public RatingDTO(string comment, string positives, string negatives, byte rating)
+        public string Subject { get; set; }
+
+        public RatingDTO(string comment, string positives, string negatives, byte rating, int teacher, string subject)
         {
             Comment = comment;
             Positives = positives;
             Negatives = negatives;
             Rating = rating;
+            Teacher = teacher;
+            Subject = subject;
         }
     }
 }
--- Commands/MunihubCommandsModule.cs
+++ Commands/MunihubCommandsModule.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,35 +8,95 @@ using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using System.Text.Json;
+using System.Threading;
+using DSharpPlus.Interactivity;
+using RestSharp;
 
 namespace csharpbot.Commands
 {
     public class MunihubCommandsModule : IModule
     {
         [Command("rate-subject")]
-        [Description("Leave reating of subject")]
+        [Description("Leave rating of subject")]
         public async Task RateSubject(CommandContext ctx, string code, byte rating, params string[] content)
+            => await Rate(ctx, 0, code, rating, content);
+
+        [Command("rate-teacher")]
+        [Description("Leave rating of teacher")]
+        public async Task RateTeacher(CommandContext ctx, int uco, byte rating, params string[] content)
+            => await Rate(ctx, uco, "", rating, content);
+        
+        public async Task Rate(CommandContext ctx, int uco, string code, byte rating, params string[] content)
         {
             var positives = new List<string>();
             var negatives = new List<string>();
             var comment = new List<string>();
             ParseContent(content, positives, negatives, comment);
+
+            var whome = uco == 0 ? code : uco.ToString();
             
             var builder = new DiscordEmbedBuilder()
             {
                 Color = DiscordColor.Green,
                 Description = printableContent(positives, negatives, comment, rating),
                 Timestamp = DateTimeOffset.Now,
-                Title = $"Raing for {code}"
+                Title = $"Raing for {whome}"
             };
             
             
-            await ctx.RespondAsync(embed: builder.Build());
+            var ratingMessage = await ctx.RespondAsync(embed: builder.Build());
 
             RatingDTO ratingObj = new RatingDTO(string.Join(' ', comment), string.Join(';', positives),
-                string.Join(';', negatives), rating);
-            await ctx.RespondAsync(JsonSerializer.Serialize<RatingDTO>(ratingObj));
+                string.Join(';', negatives), rating, uco, code);
+            var jsonRating = JsonSerializer.Serialize<RatingDTO>(ratingObj);
+            await ctx.RespondAsync(jsonRating);
+            
+            var optionReactions = new[] {DiscordEmoji.FromName(ctx.Client, ":thumbsup:"), 
+                DiscordEmoji.FromName(ctx.Client, ":thumbsdown:")
+            };
+            foreach (var t in optionReactions)
+            {
+                await ratingMessage.CreateReactionAsync(t).ConfigureAwait(false);
+            }
+            var interaction = ctx.Client.GetInteractivityModule();
+            Thread.Sleep(5000);
+            var result = await interaction
+                .WaitForMessageReactionAsync(ratingMessage, ctx.Member, new TimeSpan(0,0,20));
+            var results = result.Emoji;
+            
+            if (results.GetDiscordName() == ":+1:")
+            {
+                var response = PostRating(ratingObj);
+                await ctx.RespondAsync(response.StatusCode + response.Content + response.ErrorMessage);
+                await ratingMessage.DeleteAsync();
+            }
+            else if (results.GetDiscordName() == ":-1:")
+            {
+                //aaaaaaaaa why the fuck u wont delete ???
+                await ctx.Channel.DeleteMessageAsync(ratingMessage);
+            }
+            //Thread.Sleep(5000);
+            //await ctx.Message.DeleteAsync();
+
+
             //ctx.Message.DeleteAsync("shouldnt be there");
+        }
+
+        private IRestResponse PostRating(RatingDTO rating)
+        {
+            string json = JsonSerializer.Serialize<RatingDTO>(rating);
+            var client = new RestClient("https://localhost:5001/api/rating");
+            client.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", json,  ParameterType.RequestBody);
+            return client.Execute(request);
+            
+            /*using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }*/
         }
 
         private string printableContent(List<string> positives, List<string> negatives, List<string> comment, byte rating)
